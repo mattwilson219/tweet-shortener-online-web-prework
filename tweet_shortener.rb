1a00bdfb35eeebc1011e3da62a8b9bb24ff5c556
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-12 19:52:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 def word_substituter(tweet)
   tweet.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
-      word = dictionary
+      word = dictionary[word.downcase]
 end
 
 
